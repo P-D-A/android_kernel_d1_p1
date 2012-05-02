492d8523afc1d42f69a34f321abf5458fcf95894
Colin Cross <ccross@android.com>
2012-05-02 13:47:47 -0700

    HACK: input: evdev: make CLOCK_MONOTONIC the default
    Hack to maintain compatibility with existing Android userspace.
    
    Change-Id: Ia6d768b3d51553bb43e7f70e605c2996a874e8cb
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -330,6 +330,7 @@ static int evdev_open(struct inode *inode, struct file *file)
 		}
 	}
 
+	client->clkid = CLOCK_MONOTONIC;
 	client->bufsize = bufsize;
 	spin_lock_init(&client->buffer_lock);
 	snprintf(client->name, sizeof(client->name), "%s-%d",
