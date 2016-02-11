3ec627416ff0d78f5797cbdebcb2f9d0d2ac4427
ShevT <shev_t@mail.ru>
2016-02-11 10:30:39 +0300

    omap: board-4430sdp: remove memblock reservations

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -2898,8 +2898,6 @@ static struct sgx_omaplfb_platform_data blaze_omaplfb_plat_data = {
 	omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
 			OMAP_RAM_CONSOLE_SIZE_DEFAULT);
 
-	/* do the static reservations first */
-	memblock_remove(PHYS_ADDR_DUCATI_MEM, PHYS_ADDR_DUCATI_SIZE);
 	/* ipu needs to recognize secure input buffer area as well */
 	omap_ipu_set_static_mempool(PHYS_ADDR_DUCATI_MEM,
 					PHYS_ADDR_DUCATI_SIZE +
