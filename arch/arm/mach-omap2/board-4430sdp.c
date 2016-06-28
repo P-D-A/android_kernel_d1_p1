2a7a74018f473222bf75626d3e59fceb5ad298c3
ShevT <shev_t@mail.ru>
2016-06-28 18:10:39 +0300

    board-4430sdp: ifdef CONFIG_CMA

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -2897,6 +2897,12 @@ static struct sgx_omaplfb_platform_data blaze_omaplfb_plat_data = {
 	omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
 			OMAP_RAM_CONSOLE_SIZE_DEFAULT);
 
+#ifndef CONFIG_CMA
+	/* do the static reservations first */
+	memblock_remove(PHYS_ADDR_SMC_MEM, PHYS_ADDR_SMC_SIZE);
+	memblock_remove(PHYS_ADDR_DUCATI_MEM, PHYS_ADDR_DUCATI_SIZE);
+#endif
+
 	/* ipu needs to recognize secure input buffer area as well */
 	omap_ipu_set_static_mempool(PHYS_ADDR_DUCATI_MEM,
 					PHYS_ADDR_DUCATI_SIZE +
