7c678eba4f11b45fd426ffa72e4719d60cd092ae
ShevT <shev_t@mail.ru>
2016-07-08 16:15:25 +0300

    Revert "perf_events: Fix perf buffer watermark setting"
    This reverts commit 6a0322fa95943bdc56d16937ac2da817f6bd2c39.

--- kernel/events/ring_buffer.c
+++ kernel/events/ring_buffer.c
@@ -199,15 +199,13 @@ void perf_output_end(struct perf_output_handle *handle)
 	struct perf_event *event = handle->event;
 	struct ring_buffer *rb = handle->rb;
 
-	if (handle->sample && !event->attr.watermark) {
-		int wakeup_events = event->attr.wakeup_events;
+	int wakeup_events = event->attr.wakeup_events;
 
-		if (wakeup_events) {
-			int events = local_inc_return(&rb->events);
-			if (events >= wakeup_events) {
-				local_sub(wakeup_events, &rb->events);
-				local_inc(&rb->wakeup);
-			}
+	if (handle->sample && wakeup_events) {
+		int events = local_inc_return(&rb->events);
+		if (events >= wakeup_events) {
+			local_sub(wakeup_events, &rb->events);
+			local_inc(&rb->wakeup);
 		}
 	}
 
